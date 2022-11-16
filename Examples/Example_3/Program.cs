b7cca2f46e0da0d139353bd2a70f437b14db0a65
Roman <roman.tsomka@yndex.ru>
2022-11-16 21:45:45 +1000

    random

--- Examples/Example_3/Program.cs
+++ Examples/Example_3/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 32;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
 int numberB = 5;
-Console.WriteLine(numberA + numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
